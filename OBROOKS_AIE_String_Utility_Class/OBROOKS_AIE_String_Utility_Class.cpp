37b496f03b93ad63b90919e7921299d665cabf3a
a-b-c-d-e-f-go <158417144+a-b-c-d-e-f-go@users.noreply.github.com>
2024-02-08 11:46:41 +1100

    Added ToLower() and ToUpper()

--- OBROOKS_AIE_String_Utility_Class/OBROOKS_AIE_String_Utility_Class.cpp
+++ OBROOKS_AIE_String_Utility_Class/OBROOKS_AIE_String_Utility_Class.cpp
@@ -7,15 +7,15 @@
 class String
 {
     public: //External Constructors
-        String()
+        String() //No input.
         {
             cstr = "";
         }
-        String(const char* _str)
+        String(const char* _str) //Input is another cstr.
         {
             cstr = _str;
         }
-        String(const String& _other)
+        String(const String& _other) //Input is another string.
         {
             cstr = _other.CStr();
         }
@@ -25,8 +25,11 @@ class String
         {
             return strlen(CStr()); //Length of the char array.
         }
-        char& CharacterAt(size_t _index);
-        const char& CharacterAt(size_t _index) const
+        char& CharacterAt(size_t _index)
+        {
+            return (char&)CCharacterAt(_index); //Returns the CCharacterAt as a non-constant.
+        }
+        const char& CCharacterAt(size_t _index) const //Const version of CharacterAt, for all your const needs.
         {
             return CStr()[_index]; //Array index in the char array.
         }
@@ -36,9 +39,42 @@ class String
         }
         String& Append(const String& _str);
         String& Prepend(const String& _str);
-        const char* CStr() const;
-        String& ToLower();
-        String& ToUpper();
+        const char* CStr() const
+        {
+            return cstr; //Returns internal variable. This way, it is encapsulated. Not sure why you would want to do this when there's an = operator that changes it anyway, but here you go.
+        }
+        String& ToLower()
+        {
+            signed char offset = 'a' - 'A'; //Offset from capital to lowercase.
+            const unsigned int size = sizeof(cstr) / sizeof(cstr[0]); //Size of the original cstring's array.
+            char newstr[size] = ""; //New cstr that can be modified.
+
+            for (unsigned int i = 0; i < size; i++)
+            {
+                newstr[i] = cstr[i]; //Transfer character to new cstr.
+                if (cstr[i] >= 'A' && cstr[i] <= 'Z') //If capital letter.
+                {
+                    newstr[i] += offset; //Convert capital to lowercase.
+                }
+            }
+            return (String&)String(newstr);
+        }
+        String& ToUpper()
+        {
+            signed char offset = 'A' - 'a'; //Offset from lowercase to capital.
+            const unsigned int size = sizeof(cstr) / sizeof(cstr[0]); //Size of the original cstring's array.
+            char newstr[size] = ""; //New cstr that can be modified.
+
+            for (unsigned int i = 0; i < size; i++)
+            {
+                newstr[i] = cstr[i]; //Transfer character to new cstr.
+                if (cstr[i] >= 'a' && cstr[i] <= 'z') //If lowercase letter.
+                {
+                    newstr[i] += offset; //Convert lowercase to capital.
+                }
+            }
+            return (String&)String(newstr);
+        }
         size_t Find(const String& _str);
         size_t Find(size_t _startIndex, const String& _str);
         String& Replace(const String& _find, const String& _replace);
@@ -66,7 +102,7 @@ class String
         }
         const char& operator[](size_t _index) const
         {
-            return CharacterAt(_index); //Individual character.
+            return CCharacterAt(_index); //Individual character.
         }
     private: //Internal Variables
         const char* cstr;
